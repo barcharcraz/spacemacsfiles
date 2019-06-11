0da5b5a39244be0b0bb744c9416d1e89a37ccc1b
Charles Barto <chbarto@chbarto-linux.guest.corp.microsoft.com>
2019-06-11 11:10:50 -0700

    try and get cxx working

--- init.el
+++ init.el
@@ -8,6 +8,7 @@
                                        yaml
                                        javascript
                                        c-c++
+                                       lsp
                                        cmake
                                        emacs-lisp
                                        git
@@ -29,7 +30,8 @@
    dotspacemacs-default-font '("Source Code Pro"
                                :weight normal
                                :width normal
-                               :powerline-scale 1.1)))
+                               :powerline-scale 1.1)
+   c-c++-enable-clang-support t))
 
 (defun dotspacemacs/user-config ()
   ;(setq projectile-generic-command "fd . -0")
@@ -38,3 +40,17 @@
   (setq ispell-program-name "aspell")
   (setq cmake-enable-cmake-ide-support t))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode toml-mode racer nim-mode flycheck-nimsuggest commenter epc ctable concurrent deferred helm-company helm-c-yasnippet fuzzy flycheck-rust flycheck-nim company-tern dash-functional tern company-statistics company-c-headers company cargo markdown-mode rust-mode auto-yasnippet ac-ispell auto-complete ws-butler winum which-key web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline smeargle restart-emacs rainbow-delimiters popwin persp-mode pcre2el paradox orgit org-bullets open-junk-file neotree move-text magit-gitflow macrostep lorem-ipsum livid-mode linum-relative link-hint json-mode js2-refactor js-doc indent-guide hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-ag google-translate golden-ratio gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ flyspell-correct-helm flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu elisp-slime-nav dumb-jump disaster diminish diff-hl define-word column-enforce-mode coffee-mode cmake-mode clean-aindent-mode clang-format auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
